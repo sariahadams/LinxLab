0631eb1e07761f5563227f6ca02bed1ef6102529
Sariah Adams <108815131+sariahadams@users.noreply.github.com>
2023-04-24 13:27:23 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,20 +1,22 @@
-#The purpose of this program is to encrypt a given message using the Caesar Cipher technique
+#The purpose of this program is to encrypt a given message using the Caesar Cipher technique. This is an assignment for Linux Lab.
 
 import sys
 #import sys
 
-def caesar_cipher(message, shift):
-      result = ""
-      for i in range(len(message)):
+def caesar_cipher(message, shift):    #takes in two parameters, message and shift.
+      result = ""                      #utilizes an empty string which stores encrpyted message
+      for i in range(len(message)):     #iterates over each letter in the 'message'
           char = message[i]
-          if char.isupper():
+          if char.isupper():             #evaluates if letters are uppercase and shitfts accordingly
               result += chr((ord(char) + shift - 65) % 26 + 65)
-          elif char.islower():
+          elif char.islower():         #evaluates if letters are lowercase and shifts accordingly  
               result += chr((ord(char) + shift - 97) % 26 + 97)
           else:
               result += char
       return result
 
-for line in sys.stdin:
-  print(caesar_cipher(line, 3))
+shift = int(input("Enter shift value:")   #person can choose the shift value to encryt the message
+            
+for line in sys.stdin:    #reads the lines of the given input 
+  print(caesar_cipher(line, shift))     #prints the results of now encrypted string 
 
